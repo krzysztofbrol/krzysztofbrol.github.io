65eb760d8753d85a46fdf1193aef33a17f06e30f
Krzysztof Brol <krzysztof.brol@edu.uekat.pl>
2019-11-11 23:36:20 +0100

    fix: js fetch repositories from github api

--- src/js/main.js
+++ src/js/main.js
@@ -12,24 +12,24 @@ fetch('https://api.github.com/users/krzysztofbrol/repos?sort=updated&direction=d
       const {description, homepage, html_url, name} = repo;
       console.log(repo);
       list.innerHTML += `
-    <li class="project">
-            <div class="project__container">
-              <img class="project__logo" src="assets/img/github.svg" alt="">
-              <h3 class="project__title">${name}</h3>
-              ${
-                description ? `<p class="project__description">${description}</p>` : 'No description'
-              }
+      <li class="project">
+        <div class="project__container">
+          <img class="project__logo" src="assets/img/github.svg" alt="">
+          <h3 class="project__title">${name}</h3>
+          ${
+            description ? `<p class="project__description">${description}</p>` : 'No description'
+          }
               
-            </div>
-            <div class="project__footer">
-            ${
-              homepage ? '<a class="project__link" href="${homepage}" target="_blank" rel="nofollow noreferrer" title="Demo: ${name}.">Demo</a>' : ''
-            }
+        </div>
+        <div class="project__footer">
+          ${
+           homepage ? `<a class="project__link" href="${homepage}" target="_blank" rel="nofollow noreferrer" title="Demo: ${name}.">Demo</a>` : ''
+          }
               
-              <a class="project__link project__link--code" href="${html_url}" target="_blank" rel="nofollow noreferrer" title="Source code: ${name}.">Github</a>
-            </div>
-          </li>
-          `;
+          <a class="project__link project__link--code" href="${html_url}" target="_blank" rel="nofollow noreferrer" title="Source code: ${name}.">Github</a>
+        </div>
+      </li>
+      `;
     }
   })
   .catch(err => {
